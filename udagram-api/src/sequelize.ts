c00769bf8d18f9554f0504f63a161278ef0b65ac
nesspll <nesim@cuburn.com>
2022-06-13 20:50:35 +0200

    update

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -6,29 +6,10 @@ import { config } from "./config/config";
    password: config.password,
    database: config.database,
    host: config.host,
-   port: config.port_db,
+   port: config.port,
+
    dialect: "postgres",
    storage: ":memory:",
-   retry: {
-    match: [
-        /ETIMEDOUT/,
-        /EHOSTUNREACH/,
-        /ECONNRESET/,
-        /ECONNREFUSED/,
-        /ETIMEDOUT/,
-        /ESOCKETTIMEDOUT/,
-        /EHOSTUNREACH/,
-        /EPIPE/,
-        /EAI_AGAIN/,
-        /SequelizeConnectionError/,
-        /SequelizeConnectionRefusedError/,
-        /SequelizeHostNotFoundError/,
-        /SequelizeHostNotReachableError/,
-        /SequelizeInvalidConnectionError/,
-        /SequelizeConnectionTimedOutError/
-    ],
-    max: 5
-    }
  });
 
 // export const sequelize = new Sequelize('postgres://postgres:postgres@database.cvs2bdqzrosf.us-east-1.rds.amazonaws.com:5432/postgres')
