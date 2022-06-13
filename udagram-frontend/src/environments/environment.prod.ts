f56078553c065e9b21ed269a5740a188625fd84c
nesspll <nesim@cuburn.com>
2022-06-13 12:20:51 +0200

    update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-m7rmmhm3.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api-dev.eba-ryyahzdg.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: "http://udagram-api-dev.eba-m7rmmhm3.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api-dev.eba-ryyahzdg.us-east-1.elasticbeanstalk.com/api/v0",
   // apiHost: 'http://localhost:3000/api/v0'
 };
 
