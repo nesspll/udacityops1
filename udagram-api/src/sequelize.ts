c6487ed8719fc4015d9215734b169903344e11ae
nesspll <nesim@cuburn.com>
2022-06-13 20:37:36 +0200

    update

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,35 +1,35 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-//  export const sequelize = new Sequelize({
-//    username: config.username,
-//    password: config.password,
-//    database: config.database,
-//    host: config.host,
-//    port: config.port_db,
-//    dialect: "postgres",
-//    storage: ":memory:",
-//    retry: {
-//     match: [
-//         /ETIMEDOUT/,
-//         /EHOSTUNREACH/,
-//         /ECONNRESET/,
-//         /ECONNREFUSED/,
-//         /ETIMEDOUT/,
-//         /ESOCKETTIMEDOUT/,
-//         /EHOSTUNREACH/,
-//         /EPIPE/,
-//         /EAI_AGAIN/,
-//         /SequelizeConnectionError/,
-//         /SequelizeConnectionRefusedError/,
-//         /SequelizeHostNotFoundError/,
-//         /SequelizeHostNotReachableError/,
-//         /SequelizeInvalidConnectionError/,
-//         /SequelizeConnectionTimedOutError/
-//     ],
-//     max: 5
-//     }
-//  });
+ export const sequelize = new Sequelize({
+   username: config.username,
+   password: config.password,
+   database: config.database,
+   host: config.host,
+   port: config.port_db,
+   dialect: "postgres",
+   storage: ":memory:",
+   retry: {
+    match: [
+        /ETIMEDOUT/,
+        /EHOSTUNREACH/,
+        /ECONNRESET/,
+        /ECONNREFUSED/,
+        /ETIMEDOUT/,
+        /ESOCKETTIMEDOUT/,
+        /EHOSTUNREACH/,
+        /EPIPE/,
+        /EAI_AGAIN/,
+        /SequelizeConnectionError/,
+        /SequelizeConnectionRefusedError/,
+        /SequelizeHostNotFoundError/,
+        /SequelizeHostNotReachableError/,
+        /SequelizeInvalidConnectionError/,
+        /SequelizeConnectionTimedOutError/
+    ],
+    max: 5
+    }
+ });
 
-export const sequelize = new Sequelize('postgres://postgres:postgres@database.cvs2bdqzrosf.us-east-1.rds.amazonaws.com:5432/postgres')
+// export const sequelize = new Sequelize('postgres://postgres:postgres@database.cvs2bdqzrosf.us-east-1.rds.amazonaws.com:5432/postgres')
 
