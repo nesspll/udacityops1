78153b25f15a08bc7da482e1dccdd4eba8aa69ec
nesspll <nesim@cuburn.com>
2022-06-13 21:13:07 +0200

    update

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -6,11 +6,21 @@ import { config } from "./config/config";
    password: config.password,
    database: config.database,
    host: config.host,
-   port: config.port,
+   port: config.db_port,
 
    dialect: "postgres",
    storage: ":memory:",
  });
+
+
+
+ try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
 
 // export const sequelize = new Sequelize('postgres://postgres:postgres@database.cvs2bdqzrosf.us-east-1.rds.amazonaws.com:5432/postgres')
 
